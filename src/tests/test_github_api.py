7662411ba0bd61aa5528b415e79166a3f8aa4210
kaetaen <kaetaen@tutanota.com>
2020-12-05 00:34:01 -0300

    refactoring & test API connection

--- src/tests/test_github_api.py
+++ src/tests/test_github_api.py
@@ -1,19 +1,24 @@
 import os, sys
-
 from os.path import dirname, join, abspath
 sys.path.insert(0, abspath(join(dirname(__file__), '..')))
 
+import unittest
+
 from services.github_api import GitHubApi
 
-import unittest
 
 class TestGitHubAPI(unittest.TestCase):
     def setUp(self):
         self.api = GitHubApi()
     
     def test_API_status_not_ok(self):
-        status_code = self.api.status_code
-        self.assertEqual(status_code, 200, "The status_code doesn't ok")
+        self.assertEqual(self.api.status, 200, 'The status_code doesn\'t ok.')
+
+    def test_case_client_side_error(self):
+        self.assertFalse(self.api.status > 300 and self.api.status < 500, 'Client-side error.')
+
+    def test_case_server_side_error(self):
+        self.assertFalse(self.api.status >= 500, 'Server-side error.')
 
 
 if __name__ == '__main__':
